7ffa0976e2f52b326c26751952771c49f4162a9b
gitishdev <binaryrover@gmail.com>
2025-05-20 22:36:02 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
-
 document.addEventListener("DOMContentLoaded", () => {
     const storyList = document.getElementById("storyList");
+    if (!storyList) return;
+
     stories.forEach(story => {
         const div = document.createElement("div");
-        div.innerHTML = `<h2>${story.title}</h2>
-                         <img src="images/${story.image}" width="150">
-                         <p><a href="story.html?storyId=${story.id}&mode=full">Full Story</a> | 
-                            <a href="story.html?storyId=${story.id}&mode=page">Page by Page</a></p>`;
+        div.innerHTML = `
+            <h2>${story.title}</h2>
+            <img src="images/${story.image}" width="150">
+            <p>
+                <a href="story.html?storyId=${story.id}&mode=full">Full Story</a> |
+                <a href="story.html?storyId=${story.id}&mode=page">Page by Page</a>
+            </p>
+        `;
         storyList.appendChild(div);
     });
 });
